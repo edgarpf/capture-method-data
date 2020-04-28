741d02793a001bfc00e8e4b01dfc06aa93f241e6
Edgar Peixoto <edgar.peixoto@infobec.ca>
2020-04-28 15:45:36 -0300

    update

---
+++ src/main/java/com/evoluum/capturemethoddata/CaptureMethodData.java
@@ -0,0 +1,13 @@
+package com.evoluum.capturemethoddata;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface CaptureMethodData {
+	public String mode() default CaptureMethodValues.ALL;
+}
+
---
+++ src/main/java/com/evoluum/capturemethoddata/CaptureMethodValues.java
@@ -0,0 +1,11 @@
+package com.evoluum.capturemethoddata;
+
+public abstract class CaptureMethodValues {
+	public static final String RESPONSE = "response";
+	public static final String PARAMS = "parameters";
+	public static final String ALL = "all";
+	
+	private CaptureMethodValues() {
+		
+	}
+}
