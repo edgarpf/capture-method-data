4739837d36e3300388eb2f7c609b70715056c011
Edgar Peixoto <edgar.peixoto@infobec.ca>
2020-04-29 13:43:06 -0300

    update

--- src/main/java/com/evoluum/capturemethoddata/ElasticCaptureMethodDataConfiguration.java
+++ src/main/java/com/evoluum/capturemethoddata/ElasticCaptureMethodDataConfiguration.java
@@ -5,6 +5,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = {"com.evoluum.capturemethoddata"})
-public class AppConfiguration {
+public class ElasticCaptureMethodDataConfiguration {
 
 }
